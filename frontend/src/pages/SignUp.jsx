e1c9ade8f8a8d2ec5c128049912e196f4c0f022c
umar <23100206@lums.edu.pk>
2022-11-24 19:33:43 +0500

    fixing rebase error

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-<<<<<<< HEAD
 import axios from "axios";
-=======
->>>>>>> c8ff55b3 (created front-end and back-end dir)
 
 const SignUp = () => {
   const formik = useFormik({
@@ -26,12 +23,9 @@ const SignUp = () => {
 
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
-<<<<<<< HEAD
       axios.post("http://localhost:3001/signup", values).then((res) => {
         console.log(res);
       });
-=======
->>>>>>> c8ff55b3 (created front-end and back-end dir)
     },
   });
   console.log(formik.errors);
@@ -44,8 +38,6 @@ const SignUp = () => {
           className="flex flex-col "
           onSubmit={formik.handleSubmit}
         >
-<<<<<<< HEAD
-<<<<<<< HEAD
           <label
             htmlFor="name"
             className={`text-gray-900 block pb-2 ${
@@ -64,8 +56,6 @@ const SignUp = () => {
             onChange={formik.handleChange}
             value={formik.values.name}
           />
-=======
->>>>>>> e1a606c8 (styling signup page)
           <label
             htmlFor="email"
             className={`text-gray-900 block pb-2 ${
@@ -76,18 +66,10 @@ const SignUp = () => {
               ? formik.errors.email
               : "Email:"}
           </label>
-<<<<<<< HEAD
-=======
-          <label htmlFor="email">Email</label>
->>>>>>> c8ff55b3 (created front-end and back-end dir)
-=======
->>>>>>> e1a606c8 (styling signup page)
           <input
             type="email"
             name="email"
             id="email"
-<<<<<<< HEAD
-<<<<<<< HEAD
             className="ml-2 border border-gray-400 rounded "
             onChange={formik.handleChange}
             value={formik.values.email}
@@ -104,31 +86,6 @@ const SignUp = () => {
               ? formik.errors.password
               : "Password:"}
           </label>
-=======
-            className="ml-2 border border-gray-400 rounded"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          />
-          <label htmlFor="password">Password</label>
->>>>>>> c8ff55b3 (created front-end and back-end dir)
-=======
-            className="ml-2 border border-gray-400 rounded "
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          />
-          <label
-            htmlFor="password"
-            className={`text-gray-900 block pb-2 ${
-              formik.touched.password && formik.errors.password
-                ? "text-red-400"
-                : ""
-            }`}
-          >
-            {formik.touched.password && formik.errors.password
-              ? formik.errors.password
-              : "Password:"}
-          </label>
->>>>>>> e1a606c8 (styling signup page)
           <input
             type="password"
             name="password"
@@ -137,38 +94,10 @@ const SignUp = () => {
             onChange={formik.handleChange}
             value={formik.values.password}
           />
-<<<<<<< HEAD
-<<<<<<< HEAD
 
           <button
             type="submit"
             className="bg-blue-500 text-white rounded mt-4 mx-auto p-2 sm:w-2/4 hover:bg-blue-800"
-=======
-          <label htmlFor="name">Name</label>
-=======
-          <label
-            htmlFor="name"
-            className={`text-gray-900 block pb-2 ${
-              formik.touched.name && formik.errors.name ? "text-red-400" : ""
-            }`}
-          >
-            {formik.touched.name && formik.errors.name
-              ? formik.errors.name
-              : "Name:"}
-          </label>
->>>>>>> e1a606c8 (styling signup page)
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="ml-2 border border-gray-400 rounded"
-            onChange={formik.handleChange}
-            value={formik.values.name}
-          />
-          <button
-            type="submit"
-            className="bg-blue-500 text-white rounded mt-4 mx-auto p-2 sm:w-2/4 "
->>>>>>> c8ff55b3 (created front-end and back-end dir)
           >
             Submit
           </button>
