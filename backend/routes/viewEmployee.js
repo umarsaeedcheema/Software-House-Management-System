0ccde7c1a41b95020deb5e09ea592418a097b864
umar <23100206@lums.edu.pk>
2022-11-30 22:17:44 +0500

    view employee data

--- backend/routes/viewEmployee.js
+++ backend/routes/viewEmployee.js
@@ -22,30 +22,15 @@ connection.connect((err) => {
 // // parsing data
 router.use(express.json());
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
-  const first_name = req.body.first_name;
-  const last_name = req.body.last_name;
-  const designation = req.body.designation;
-  const salary = req.body.salary;
-  const hired_on = req.body.hired_date;
-
-  connection.query(
-    {
-      sql: "INSERT INTO employee (first_name, last_name, designation, salary, hired_on) VALUES (?, ?, ?,?,?)",
-      values: [first_name, last_name, designation, salary, hired_on],
-      timeout: 40000, //40s
-    },
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(result);
-      }
+router.get("/", (req, res) => {
+  connection.query("SELECT * FROM employee", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Success");
+      res.send(result);
     }
-  );
+  });
 });
 
 module.exports = router;
-//
