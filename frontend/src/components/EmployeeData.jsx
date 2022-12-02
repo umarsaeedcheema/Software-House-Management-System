5ee35139109fb85cb2208aa2faf27f858e574323
AbdullahAhmadAAK <abdullahaak06@gmail.com>
2022-12-02 17:46:13 +0500

    partially implement show more details of employee
    active projects, past projects, supervisors still left. currently shows employee id, hire date and salary

--- frontend/src/components/EmployeeData.jsx
+++ frontend/src/components/EmployeeData.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 
 const EmployeeData = () => {
   const [employees, setEmployees] = useState([]);
+  const [showEmployeeDetails, setShowEmployeeDetails] = useState(false)
+
   useEffect(() => {
     axios.get("http://localhost:3001/viewEmployee").then((response) => {
       setEmployees(response.data);
@@ -17,11 +19,29 @@ const EmployeeData = () => {
             {employee.first_name} {employee.last_name}
           </h1>
           <h1>{employee.designation}</h1>
+          
+          {showEmployeeDetails? <p>Employee ID: {employee.id} </p> : null}
+          {showEmployeeDetails? <p>Salary: {employee.salary} </p> : null}
+          {showEmployeeDetails? <p>Hired on: {employee.hired_on} </p> : null}
+
+          <button onClick={() => setShowEmployeeDetails(!showEmployeeDetails)} className="bg-yellow-300">View More Details</button>
+          <button type="submit"> Submit </button>
+          <button className="bg-red-300">Create Review</button>
+
+        {/* active projects to be done */}
+        {/*  past projects */}
+        {/* supervision */}
+
+          
+
+        {/* yearly reviews */}
         </div>
       </div>
     );
   });
   return <div className="flex gap-3">{employeeData}</div>;
+
+  
 };
 
 export default EmployeeData;
